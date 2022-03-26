6a08066ed99ba0bc64cd505c3330641b3aed695e
JemalAbdullahi <53106252+JemalAbdullahi@users.noreply.github.com>
2022-03-26 14:42:52 +0000

    Deploying to gh-pages from  @ 2ada9cf9214721b6ee5a80ba96f95334e27997a5 🚀

--- src/components/header/header.data.js
+++
@@ -1,9 +0,0 @@
-const data = [
-  { path: "/", label: "Home" },
-  { path: "/about", label: "About" },
-  { path: "/services", label: "Services" },
-  // { path: "/reviews", label: "Reviews" },
-  { path: "/contact", label: "Contact Us" },
-];
-
-export default data;
